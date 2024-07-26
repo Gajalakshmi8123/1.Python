4ebe985d4a1f3af69e8447fa2199390814dfacec
Gajalakshmi8123 <168989953+Gajalakshmi8123@users.noreply.github.com>
2024-07-26 22:13:54 +0530

    Add files via upload

---
+++ Class_Function.py
@@ -0,0 +1,50 @@
+class Multiple:
+    def eligible():
+        Gender = input("Your Gender: ")
+        Age = int(input("Your Age: "))
+        if Gender == 'Male' and Age >= 21:
+            print("ELIGIBLE")
+        elif Gender == 'Female' and Age >= 18:
+            print("ELIGIBLE")
+        else:
+            print("NOT ELIGIBLE")
+    
+    def percentage():
+        Subject1 = int(input("Enter the marks for Subject 1: "))
+        Subject2 = int(input("Enter the marks for Subject 2: "))
+        Subject3 = int(input("Enter the marks for Subject 3: "))
+        Subject4 = int(input("Enter the marks for Subject 4: "))
+        Subject5 = int(input("Enter the marks for Subject 5: "))
+        Total = Subject1 + Subject2 + Subject3 + Subject4 + Subject5
+        Percentage = Total / 5
+        print("Total:", Total)
+        print("Percentage:", Percentage)          
+    
+    
+    def triangle1():
+        Height = int(input("Enter the height of the triangle: "))
+        Breadth = int(input("Enter the base of the triangle: "))
+        Area_formula = (Height * Breadth) / 2
+        print("Area of Triangle:", Area_formula)
+        
+        Height1 = int(input("Enter the first side of the triangle: "))
+        Height2 = int(input("Enter the second side of the triangle: "))
+        Breadth = int(input("Enter the third side of the triangle: "))
+        Perimeter_Triangle = Height1 + Height2 + Breadth
+        print("Perimeter of Triangle:", Perimeter_Triangle)
+    
+    
+    def odd_even():
+        Number = int(input("Enter the number: "))
+        if Number % 2 == 0:
+            print(Number, "is an Even number")
+        else:
+            print(Number, "is an Odd number")
+    
+    
+    
+
+
+
+
+
